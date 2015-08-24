9564e7ae3c4e9a9c8b1d207d1d63035d90bf3fc0
Andy Holt <andrew.holt@hotmail.co.uk>
2015-08-24 18:52:49 +0100

    Update article setup script for OSX compatibility.
    Syntax of sed differs for standard installation on OS X compared to
    Linux. Detect system type and issue correct commands.

--- ArticleSetupDirectory.sh
+++ ArticleSetupDirectory.sh
@@ -3,6 +3,19 @@
 echo "Setting up for article writing in directory $PWD."
 projectname=`basename "$PWD"`
 
+# get operating system type
+# different sed syntax for linux and mac
+if [[ `uname` == 'Linux' ]]; then
+    export LINUX=1
+else
+    export LINUX=0
+fi
+if [[ `uname` == 'Darwin' ]]; then
+    export OSX=1
+else
+    export OSX=0
+fi
+
 ## Not needed when adharticle is installed
 # echo "Getting adharticle class..."
 # ln -s ~/Projects/LaTeXClasses/adharticle.cls $PWD/.
@@ -16,14 +29,23 @@ echo "Done"
 echo "Copying Makefile..."
 cp ~/Projects/WritingTools/ArticleMakefile $PWD/Makefile
 echo "Editing Makefile..."
-sed -i "s#myfile#$projectname#gi" Makefile
-sed -i 's#mybib#Theology#gi' Makefile
+if [[ $LINUX -eq 1 ]]; then
+    sed -i "s#myfile#$projectname#gi" Makefile
+    sed -i 's#mybib#Theology#gi' Makefile
+elif [[ $OSX -eq 1 ]]; then
+    sed -i '' -e "s#myfile#$projectname#g" Makefile
+    sed -i '' -e 's#mybib#Theology#g' Makefile
+fi
 echo "Done"
 
 echo "Copying Guardfile..."
 cp ~/Projects/WritingTools/ArticleGuardfile $PWD/Guardfile
 echo "Editing Guardfile"
-sed -i "s#myfile#$projectname#gi" Guardfile
+if [[ $LINUX -eq 1 ]]; then
+    sed -i "s#myfile#$projectname#gi" Guardfile
+elif [[ $OSX -eq 1 ]]; then
+    sed -i '' -e "s#myfile#$projectname#g" Guardfile
+fi
 echo "Done"
 
 echo "Create markdown file"
