56303f2e456f741d4546fdab7e83001ef04a4903
Andy Holt <andrew.holt@hotmail.co.uk>
2015-08-24 18:58:55 +0100

    Fix sermon setup script for OSX compatibility.

--- SermonSetupDirectory.sh
+++ SermonSetupDirectory.sh
@@ -3,6 +3,19 @@
 echo "Setting up for sermon in directory $PWD."
 projectname=`basename "$PWD"`
 
+# get operating system type
+# different sed syntax for linux and mac
+if [[ `uname` == 'Linux' ]]; then
+    export LINUX=1
+else
+    export LINUX=0
+fi
+if [[ `uname` == 'Darwin' ]]; then
+    export OSX=1
+else
+    export OSX=0
+fi
+
 ## Not needed when adharticle is installed
 # echo "Getting adharticle class..."
 # ln -s ~/Projects/LaTeXClasses/adharticle.cls $PWD/.
@@ -12,13 +25,21 @@ projectname=`basename "$PWD"`
 echo "Copying Makefile..."
 cp ~/Projects/WritingTools/SermonMakefile $PWD/Makefile
 echo "Editing Makefile..."
-sed -i "s#myfile#$projectname#gi" Makefile
+if [[ $LINUX -eq 1 ]]; then
+    sed -i "s#myfile#$projectname#gi" Makefile
+elif [[ $OSX -eq 1 ]]; then
+    sed -i '' -e "s#myfile#$projectname#g" Makefile
+fi
 echo "Done"
 
 echo "Copying Guardfile..."
 cp ~/Projects/WritingTools/SermonGuardfile $PWD/Guardfile
 echo "Editing Guardfile"
-sed -i "s#myfile#$projectname#gi" Guardfile
+if [[ $LINUX -eq 1 ]]; then
+    sed -i "s#myfile#$projectname#gi" Guardfile
+elif [[ $OSX -eq 1 ]]; then
+    sed -i '' -e "s#myfile#$projectname#g" Guardfile
+fi
 echo "Done"
 
 echo "Creating user files"
